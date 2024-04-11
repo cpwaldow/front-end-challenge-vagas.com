445f2df42ad7e4ebdc491ed948faed3037e488f3
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-11 19:30:57 -0300

    test: select React, add 50 qnt and submit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Submit Form', () => {
+  it('Select React option with 50 qnt', () => {
+    const storage =
+      '[{"stickers":["React"],"counter":"50","obs":"Selecionando 50 React stickers"}]';
+    cy.clearLocalStorage();
+    cy.visit('http://localhost:5173/');
+    cy.contains('React').click();
+    cy.get('#counter').clear();
+    cy.get('#counter').focus().type('50');
+    cy.get('textarea').focus().type('Selecionando 50 React stickers');
+    cy.get('#btn-submit').click();
+    cy.get('.success-submit').should('be.visible');
+
+    cy.getAllLocalStorage().then((result) => {
+      expect(result).to.deep.equal({
+        'http://localhost:5173': {
+          info: storage,
+        },
+      });
+    });
+  });
+});
