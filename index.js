08300fd46908b369c9bff4be695b788fdf7233e1
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-11 15:10:23 -0300

    feat: initial form validation to not allow empty or invalid fields

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const counterInput = document.querySelector('#counter');
-const btnSubmit = document.querySelector('#btn-submit');
+const stickersInput = document.querySelectorAll('.checkbox');
 
 const handleInputChange = () => {
   counterInput.addEventListener('input', () => {
     const counterInputValue = Number(counterInput.value);
     if (counterInputValue < 0 || !counterInputValue) {
       counterInput.classList.add('input-error');
-      btnSubmit.disabled = true;
       return;
     }
   });
@@ -32,11 +31,47 @@ const handleBtnMore = () => {
   btnMore.addEventListener('click', (e) => {
     e.preventDefault();
     const counterInputValue = Number(counterInput.value);
-
+    counterInput.classList.remove('input-error');
     document.querySelector('#counter').value = counterInputValue + 1;
   });
 };
 
-handleInputChange();
-handleBtnLess();
-handleBtnMore();
+const handleErrors = () => {
+  if (Number(counterInput.value) === 0) {
+    alert('O valor do input não pode ser 0');
+    counterInput.classList.add('input-error');
+    throw new Error('O valor do input não pode ser 0');
+  }
+
+  const validateStickers = Array.from(stickersInput).some(
+    (sticker) => sticker.checked,
+  );
+
+  if (!validateStickers) {
+    alert('Selecione pelo menos uma opção de adesivo');
+    throw new Error('Selecione pelo menos uma opção de adesivo');
+  }
+};
+
+const handleSubmitForm = () => {
+  const btnSubmit = document.querySelector('#btn-submit');
+  btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    try {
+      handleErrors();
+      const inputsChecked = Array.from(
+        document.querySelectorAll('.checkbox:checked'),
+      );
+      console.log(inputsChecked);
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
+};
+
+window.onload = () => {
+  handleInputChange();
+  handleBtnLess();
+  handleBtnMore();
+  handleSubmitForm();
+};
