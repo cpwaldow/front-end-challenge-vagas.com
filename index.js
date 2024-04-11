ba95372f596659c8c4d2a22c82a94411fcc13cc2
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-11 13:53:18 -0300

    feat: interation with button more and letts quantity of items

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('ola mundo');
+const counterInput = document.querySelector('#counter');
+const btnSubmit = document.querySelector('#btn-submit');
+
+const handleInputChange = () => {
+  counterInput.addEventListener('input', () => {
+    const counterInputValue = Number(counterInput.value);
+    if (counterInputValue < 0 || !counterInputValue) {
+      counterInput.classList.add('input-error');
+      btnSubmit.disabled = true;
+      return;
+    }
+  });
+};
+
+const handleBtnLess = () => {
+  const btnLess = document.querySelector('.btn-menos');
+
+  btnLess.addEventListener('click', (e) => {
+    e.preventDefault();
+    const counterInputValue = Number(counterInput.value);
+    if (counterInputValue === 0) {
+      counterInput.classList.add('input-error');
+      return;
+    }
+    document.querySelector('#counter').value = counterInputValue - 1;
+  });
+};
+
+const handleBtnMore = () => {
+  const btnMore = document.querySelector('.btn-mais');
+
+  btnMore.addEventListener('click', (e) => {
+    e.preventDefault();
+    const counterInputValue = Number(counterInput.value);
+
+    document.querySelector('#counter').value = counterInputValue + 1;
+  });
+};
+
+handleInputChange();
+handleBtnLess();
+handleBtnMore();
